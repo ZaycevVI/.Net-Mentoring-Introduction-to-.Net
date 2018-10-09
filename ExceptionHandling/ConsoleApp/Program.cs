e497d6fe9f16be02d98b6ff598904ca9e20aea03
ZaycevVI <goldlike@mail.ru>
2018-10-09 23:13:25 +0300

    module 3

---
+++ ExceptionHandling/ConsoleApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using StringProcessing;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private const string EnterString = "Pls, enter custom string:";
+        private const string EnterNumber = "Pls, enter custom integer number:";
+        private const string ResultTemplate = "First symbols of string is: {0}";
+        private const string ResultNumberTemplate = "Result number is: {0}";
+
+        static void Main(string[] args)
+        {
+            bool repeat;
+            Action<string>[] action = { FirstSymbolWorkflow, ParseToIntWorkflow };
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Enter one point from menu below:\n1 - First symbol;\netc - Parse to integer;");
+                var menu = Console.ReadLine() == "1" ? 0 : 1;
+
+                Console.Clear();
+                Console.WriteLine(menu == 0 ? EnterString : EnterNumber);
+                var input = Console.ReadLine();
+                action[menu](input);
+                Console.WriteLine("Would you like to repeat(y - yes, else - othes)?");
+                repeat = (Console.ReadLine() == "y");
+            } while (repeat);
+        }
+
+        private static void FirstSymbolWorkflow(string input)
+        {
+            try
+            {
+                var symbol = input.FirstSymbol();
+                Console.WriteLine(ResultTemplate, symbol);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void ParseToIntWorkflow(string input)
+        {
+            try
+            {
+                var number = input.ToInt();
+                Console.WriteLine(ResultNumberTemplate, number);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/StringProcessing/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace StringProcessing
+{
+    public static class StringExtensions
+    {
+        private const int StartAsciCode = 48;
+        private const int EndAsciCode = 57;
+
+        public static char FirstSymbol(this string input)
+        {
+            if(string.IsNullOrEmpty(input))
+                throw new ArgumentException(
+                    "Input string is null or empty.", nameof(input));
+
+            return input[0];
+        }
+
+        public static int ToInt(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                throw new ArgumentException(
+                    "Input string is null or empty.", nameof(input));
+
+            if(input.Any(s => s < StartAsciCode || s > EndAsciCode))
+                throw new InvalidCastException("String contains invalid symbols.");
+
+            checked
+            {
+                var number = 0;
+
+                foreach (var symbol in input)
+                {
+                    number *= 10;
+                    number += symbol - StartAsciCode;
+                }
+
+                return number;
+            }
+        }
+    }
+}
