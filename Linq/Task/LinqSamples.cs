abf3f6d9c251737a8197382a1afba23147548714
Uladzimir Zaitsau <zaycevvi@mail.ru>
2018-10-15 13:02:23 +0300

    5 and 6 task fix

--- Linq/Task/LinqSamples.cs
+++ Linq/Task/LinqSamples.cs
@@ -114,12 +114,16 @@ namespace Task
                         c.CompanyName,
                         order?.OrderDate.Month,
                         order?.OrderDate.Year,
-                        order?.Total
+                        order?.Total,
+                        GoodsTurnover = c.Orders?.Sum(o => o.Total) ?? 0
                     };
                 })
-                .OrderBy(c => c.Year)
+                .OrderBy(c => c.Year == null ? 2 : 1)
+                .ThenBy(c => c.Year)
+                .ThenBy(c => c.Month == null ? 2 : 1)
                 .ThenBy(c => c.Month)
-                .ThenByDescending(c => c.Total)
+                .ThenByDescending(c => c.GoodsTurnover)
+                .ThenBy(c => c.CompanyName == null ? 2 : 1)
                 .ThenBy(c => c.CompanyName);
 
             foreach (var c in customers)
@@ -134,9 +138,9 @@ namespace Task
         public void Linq6()
         {
             var customers = _dataSource.Customers
-                .Where(c => c.PostalCode.Any(p => !char.IsDigit(p)
+                .Where(c => c.PostalCode?.Any(p => !char.IsDigit(p)
                 || string.IsNullOrEmpty(c.Region)
-                || !c.Phone.StartsWith("(")));
+                || !c.Phone.StartsWith("(")) == true);
 
             foreach (var c in customers)
             {
