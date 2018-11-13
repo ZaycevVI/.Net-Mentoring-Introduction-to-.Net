25098fb82e97f833229c98f26e7381c4f3c2b890
Uladzimir_Zaitsau <uladzimir_zaitsau1@epam.com>
2018-11-13 11:43:45 +0300

    finish sql basic module

--- Northwind/Northwind_1.1/dbo/Tables/CreditCard.sql
+++ Northwind/Northwind_1.1/dbo/Tables/CreditCard.sql
@@ -1,20 +1,9 @@
-﻿SET NOCOUNT ON
-GO
-
-USE Northwind
-GO
-
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
-		   WHERE TABLE_NAME = N'CreditCard')
-		   DROP TABLE CreditCard;
-GO
-
-CREATE TABLE [dbo].[CreditCard] (
+﻿CREATE TABLE [dbo].[CreditCard]
+(
 	[Id]             INT           NOT NULL,
 	[ExpirationDate] DATE          NULL,
 	[CardHolder]     NVARCHAR (50) NULL,
 	[EmployeeID]     INT           NULL,
 	PRIMARY KEY CLUSTERED ([Id] ASC),
 	CONSTRAINT [FK_CreditCard_ToEmployees] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employees] ([EmployeeID])
-);
-GO
+)
---
+++ Northwind/Northwind_1.3/dbo/Tables/CreditCard.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CreditCard] (
+    [Id]             INT           NOT NULL,
+    [ExpirationDate] DATE          NULL,
+    [CardHolder]     NVARCHAR (50) NULL,
+    [EmployeeID]     INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_CreditCard_ToEmployees] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employees] ([EmployeeID])
+);
+
